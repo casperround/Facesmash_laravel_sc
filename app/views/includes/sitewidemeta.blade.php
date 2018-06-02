f58f6acac552a6d7ccfb627360800e584bb42dff
casper <64666@gmail.com>
2018-06-02 21:07:30 +0100

    User content code updates

--- app/views/includes/sitewidemeta.blade.php
+++ app/views/includes/sitewidemeta.blade.php
@@ -10,3 +10,9 @@
 <meta property="og:image" content="{{ URL::to("") }}">
 <meta name="theme-color" content="#ffffff">
 <meta content="Casper Round" name="author">
+
+<!-- Compiled and minified CSS -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/css/materialize.min.css">
+
+<!-- Compiled and minified JavaScript -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/js/materialize.min.js"></script>
