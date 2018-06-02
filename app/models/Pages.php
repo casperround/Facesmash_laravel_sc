4d678810268359632420b69e8a3fb4c1450dc95a
Rod <rod@rodlucas.uk>
2018-06-02 14:48:28 +0100

    adding pages model

---
+++ app/models/Pages.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: roddy
+ * Date: 02/06/2018
+ * Time: 14:45
+ */
+
+class Pages extends Eloquent
+{
+
+    protected $table = 'pages';
+
+    protected $fillable = ["id", "uid", "unique_pagename", "owner_id", "category", "about", "website", "twitter", "facebook", "youtube", "discord_invite", "banner_path", "page_img_path"];
+
+    public $timestamps = false;
+
+}
