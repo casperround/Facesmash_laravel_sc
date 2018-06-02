8ed98aa3a07a89f9d2ff5984dbd0e2ada701c9f1
casper <64666@gmail.com>
2018-06-02 11:31:39 +0100

    new page

--- app/views/discover.blade.php
+++ app/views/discover.blade.php
@@ -1,7 +1,10 @@
 @extends('layouts.public', ["title" => "Home", "sidebar" => false])
 
 @section("content")
+    <div class="col-8" style="overflow-y:scroll;margin-top:60px;padding:10px;background:#efefef;height:100vh;">
 
 
 
+
+    </div>
 @stop
