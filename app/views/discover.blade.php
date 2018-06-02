35644599c6a9bd4d935216480ac56ada28732720
casper <64666@gmail.com>
2018-06-02 12:54:49 +0100

    Discover Page

--- app/views/discover.blade.php
+++ app/views/discover.blade.php
@@ -1,8 +1,67 @@
 @extends('layouts.public', ["title" => "Home", "sidebar" => false])
 
 @section("content")
-    <div class="col-8" style="overflow-y:scroll;margin-top:60px;padding:10px;background:#efefef;height:100vh;">
 
+    <style>
+        .Profile_Header {
+            height:100px;
+            width:100%;
+            background: url(images/cover.jpg) no-repeat center;
+
+            background-size: cover;
+        }
+        .Tabets {
+            height:40px;
+            width:100%;
+            transition: background 0.2s;
+        }
+        .Tabets:hover {
+            background: #656D78;
+            transition: background 0.2s;
+        }
+        .P_Head {
+            height:50%;
+            padding:10px;
+            position: relative;
+
+        }
+        .Profile_Img {
+            border-radius: 50px;
+            height:50px;
+            width:50px;
+            margin-left: auto;
+            margin-right:auto;
+        }
+    </style>
+    <div class="col-8" style="overflow-y:scroll;margin-top:60px;padding:10px;background:#efefef;height:100vh;">
+        <div class="jumbotron jumbotron-fluid" style="height:20%;">
+            <div class="container" style="height:40%;">
+                <h1 class="display-3">Discover</h1>
+                <p class="lead">Discover more content from people around the world!</p>
+            </div>
+        </div></br>
+        <div class="row">
+            <div class="col-4">
+                <a href="dpictures.php"><button class="btn btn-outline-info" style="width:100%;">Pictures</button></a>
+            </div>
+            <div class="col-4">
+                <a href="dvideos.php"><button class="btn btn-outline-info" style="width:100%;">Videos</button></a>
+            </div>
+            <div class="col-4">
+                <a href="dgifs.php"><button class="btn btn-outline-info" style="width:100%;">Gif's</button></a>
+            </div>
+        </div></br>
+        <div class="row">
+            <div class="col-4">
+                <a href="dpage.php"><button class="btn btn-outline-info" style="width:100%;">Pages</button></a>
+            </div>
+            <div class="col-4">
+                <a href="dgroup.php"><button class="btn btn-outline-info" style="width:100%;">Groups</button></a>
+            </div>
+            <div class="col-4">
+                <a href="dchannel.php"><button class="btn btn-outline-info" style="width:100%;">Channels</button></a>
+            </div>
+        </div></br>
 
 
 
