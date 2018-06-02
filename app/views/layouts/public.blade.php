6a630d049b3c900a6b0a0cfd27f1ba149505dfc3
casper <64666@gmail.com>
2018-06-02 14:14:45 +0100

    Added on discover section:
    Channels
    Gifs
    Groups
    Pages
    Photos
    Videos

--- app/views/layouts/public.blade.php
+++ app/views/layouts/public.blade.php
@@ -28,7 +28,7 @@
                     <a class="nav-link" href="{{ URL::route("home") }}">Home <span class="sr-only">(current)</span></a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="{{ URL::route("discover") }}" >Discover</a>
+                    <a class="nav-link" href="{{ URL::route("discover") }}">Discover</a>
                 </li>
                 @if (Auth::check())
                     <li class="nav-item">
@@ -98,6 +98,7 @@
         @include("includes.chat-container")
 </div>
 @endif
+    @yield("content")
 
 
 <script src="//code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
