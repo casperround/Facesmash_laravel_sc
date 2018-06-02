c84acd27430b76644235977b961ff2b725363d73
casper <64666@gmail.com>
2018-06-02 20:53:32 +0100

    Added channels,events,groups,pages
    Added controller, page content & routes

---
+++ app/views/events/userevent.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.public', ["title" => "My Events", "sidebar" => false])
+
+@section("content")
+    @if (Auth::check())
+        <div class="col-8" style="overflow-y:scroll;margin-top:60px;padding:10px;background:#efefef;height:100vh;">
+            @endif
+            <div class="col-md" style="overflow-y:scroll;margin-top:10px;padding:10px;background:#efefef;height:100vh;">
+                @include("includes.discover-top")
+                <div class="card-columns">
+
+                    <div class="col-md">
+                        <div class="card">
+                            <video class="card-img-top" src=""></video>
+                            <div class="card-body" style="color: black">
+                                <h4 class="card-title">Author Username | Posted Date</h4>
+                                <p class="card-text">Post Text if any</p>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+            @if (Auth::check())
+        </div>
+    @endif
+@stop
