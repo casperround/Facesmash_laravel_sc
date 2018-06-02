55a05a56e7560522f2cbbcb14dcea70820899bb2
Rod <rod@rodlucas.uk>
2018-06-02 17:59:23 +0100

    spliting and adding content to discover

--- app/views/discover.blade.php
+++ app/views/discover.blade.php
@@ -1,45 +1,12 @@
-@extends('layouts.public', ["title" => "Home", "sidebar" => false])
+@extends('layouts.public', ["title" => "Discover", "sidebar" => false])
 
 @section("content")
     @if (Auth::check())
         <div class="col-8" style="overflow-y:scroll;margin-top:60px;padding:10px;background:#efefef;height:100vh;">
     @endif
-            <div class="col-12" style="overflow-y:scroll;margin-top:60px;padding:10px;background:#efefef;height:100vh;">
-                <div class="jumbotron jumbotron-fluid" style="height:20%;">
-                    <div class="container" style="height:40%;color:black;">
-                       <h1 class="display-3">Discover</h1>
-                       <p class="lead">Discover more content from people around the world!</p>
-                    </div>
-                </div>
 
-            <br>
+            @include("includes.discover-top")
 
-            <div class="row">
-                <div class="col-4">
-                    <a href="{{ URL::route("discover.photo") }}"><button class="btn btn-outline-info" style="width:100%;">Pictures</button></a>
-                </div>
-                <div class="col-4">
-                    <a href="{{ URL::route("discover.video") }}"><button class="btn btn-outline-info" style="width:100%;">Videos</button></a>
-                </div>
-                <div class="col-4">
-                    <a href="{{ URL::route("discover.gif") }}"><button class="btn btn-outline-info" style="width:100%;">Gif's</button></a>
-                </div>
-            </div>
-
-            <br>
-
-            <div class="row">
-                <div class="col-4">
-                    <a href="{{ URL::route("discover.page") }}"><button class="btn btn-outline-info" style="width:100%;">Pages</button></a>
-                </div>
-                <div class="col-4">
-                    <a href="{{ URL::route("discover.group") }}"><button class="btn btn-outline-info" style="width:100%;">Groups</button></a>
-                </div>
-                <div class="col-4">
-                    <a href="{{ URL::route("discover.channel") }}"><button class="btn btn-outline-info" style="width:100%;">Channels</button></a>
-                </div>
-            </div>
-            <br>
                 <div class="card-columns">
                     @foreach(Posts::all() as $post)
                         <div class="col-md">
---
+++ app/views/includes/discover-top.blade.php
@@ -0,0 +1,36 @@
+<div class="col-md" style="overflow-y:scroll;margin-top:10px;padding:10px;background:#efefef;height:100vh;">
+    <div class="jumbotron jumbotron-fluid" style="height:20%;background-color: #efefef">
+        <div class="container" style="height:40%;color:black;">
+            <h1 class="display-3">Discover</h1>
+            <p class="lead">Discover more content from people around the world!</p>
+        </div>
+    </div>
+
+    <br>
+
+    <div class="row">
+        <div class="col-4">
+            <a href="{{ URL::route("discover.photo") }}"><button class="btn btn-outline-info" style="width:100%;">Pictures</button></a>
+        </div>
+        <div class="col-4">
+            <a href="{{ URL::route("discover.video") }}"><button class="btn btn-outline-info" style="width:100%;">Videos</button></a>
+        </div>
+        <div class="col-4">
+            <a href="{{ URL::route("discover.gif") }}"><button class="btn btn-outline-info" style="width:100%;">Gif's</button></a>
+        </div>
+    </div>
+
+    <br>
+
+    <div class="row">
+        <div class="col-4">
+            <a href="{{ URL::route("discover.page") }}"><button class="btn btn-outline-info" style="width:100%;">Pages</button></a>
+        </div>
+        <div class="col-4">
+            <a href="{{ URL::route("discover.group") }}"><button class="btn btn-outline-info" style="width:100%;">Groups</button></a>
+        </div>
+        <div class="col-4">
+            <a href="{{ URL::route("discover.channel") }}"><button class="btn btn-outline-info" style="width:100%;">Channels</button></a>
+        </div>
+    </div>
+    <br>
