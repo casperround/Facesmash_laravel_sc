0a089a7f3b12d08e7b2f2fbe0dc6e63684e217fb
casper <64666@gmail.com>
2018-06-03 06:43:12 +0100

    User content code updates

--- app/views/includes/sitewidemeta.blade.php
+++ app/views/includes/sitewidemeta.blade.php
@@ -19,3 +19,4 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js" type="text/javascript"></script>
 
 
+<link rel='stylesheet prefetch' href='https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css'>
