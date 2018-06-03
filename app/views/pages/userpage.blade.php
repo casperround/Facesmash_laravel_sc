941d02ddb7f6553bcef95f04c539552a8cab68bf
casper <64666@gmail.com>
2018-06-03 12:23:03 +0100

    User content code updates

--- app/views/pages/userpage.blade.php
+++ app/views/pages/userpage.blade.php
@@ -93,7 +93,7 @@
                                 <div class="card">
                                     <img class="card-img-top" src="" alt="Card image cap">
                                     <div class="card-body">
-                                        <center><p style="font-size:20px;font-weight:bold;" class="card-text">{{ $pages->unique_pagename }}</p></center>
+                                        <center><p style="font-size:20px;font-weight:bold;color:black;" class="card-text">{{ $pages->unique_pagename }}</p></center>
                                     </div>
                                 </div>
                             @endforeach
