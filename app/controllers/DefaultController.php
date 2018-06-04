13952163e9c5d126c9bd145dd380a0161934a564
casper <64666@gmail.com>
2018-06-05 00:46:10 +0100

    updated head -> title

--- app/controllers/DefaultController.php
+++ app/controllers/DefaultController.php
@@ -115,18 +115,7 @@ class DefaultController extends BaseController {
     }
 
     public function homePageFormPost() {
-        function readURL(input) {
-            if (input.files && input.files[0]) {
-                var reader = new FileReader();
-                reader.onload = function (e) {
-                    $('#blah')
-                    .attr('src', e.target.result)
-                    .width(150)
-                    .height(200);
-                };
-                reader.readAsDataURL(input.files[0]);
-            }
-        }
+
         if (Input::hasFile("file_upload")) {
 
             $uid = str_random(10);
--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -67,6 +67,22 @@
                     <div class="form-group">
                         <input class="form-control" type="file" name="file_upload">
                         <img id="blah" src="#" alt="your image" />
+                        <script>
+                            function readURL(input) {
+                                if (input.files && input.files[0]) {
+                                    var reader = new FileReader();
+
+                                    reader.onload = function (e) {
+                                        $('#blah')
+                                            .attr('src', e.target.result)
+                                            .width(150)
+                                            .height(200);
+                                    };
+
+                                    reader.readAsDataURL(input.files[0]);
+                                }
+                            }
+                        </script>
                     </div>
                 </div>
             </div>
