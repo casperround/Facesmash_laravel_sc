81c9ae1de5719b790b3071ceffae7735bde4b643
casper <64666@gmail.com>
2018-06-05 21:21:58 +0100

    channels / pages / events / groups / (Added cover & image)

--- app/views/discover.blade.php
+++ app/views/discover.blade.php
@@ -21,28 +21,28 @@
                         @endforeach
                         @foreach(Posts::where("visibility", "=", "1")->orderBy('post_time', 'DESC')->orderBy('post_date', 'DESC')->get() as $post)
 
-                        @if ($post->media_type == 'text')
-                        <div class="col-md">
-                            <div class="card">
-                                <div class="card-body" style="color: black">
-                                    <h4 class="card-title">{{{ User::where("id", "=", $post->author_id)->pluck("username") }}} | {{{ $post->post_date }}}</h4>
-                                    <p class="card-text">{{{ $post->text }}}</p>
-                                </div>
-                            </div>
-                        </div>
-                        @endif
-                        @if ($post->media_type == 'jpg' OR $post->media_type == 'png' OR $post->media_type == 'PNG' OR $post->media_type == 'JPG')
-                                <div class="col-md">
-                                    <div class="card">
-                                        <img class="card-img-top" src="{{ URL::to($post->file_path) }}" alt="Card image cap">
-                                        <div class="card-body" style="color: black">
-                                            <h4 class="card-title">{{{ User::where("id", "=", $post->author_id)->pluck("username") }}} | {{{ $post->post_date }}}</h4>
-                                            <p class="card-text">{{{ $post->text }}}</p>
-                                        </div>
+                            @if ($post->media_type == 'text')
+                            <div class="col-md">
+                                <div class="card">
+                                    <div class="card-body" style="color: black">
+                                        <h4 class="card-title">{{{ User::where("id", "=", $post->author_id)->pluck("username") }}} | {{{ $post->post_date }}}</h4>
+                                        <p class="card-text">{{{ $post->text }}}</p>
                                     </div>
                                 </div>
-                        @endif
-                    @endforeach
+                            </div>
+                            @endif
+                            @if ($post->media_type == 'jpg' OR $post->media_type == 'png' OR $post->media_type == 'PNG' OR $post->media_type == 'JPG')
+                                    <div class="col-md">
+                                        <div class="card">
+                                            <img class="card-img-top" src="{{ URL::to($post->file_path) }}" alt="Card image cap">
+                                            <div class="card-body" style="color: black">
+                                                <h4 class="card-title">{{{ User::where("id", "=", $post->author_id)->pluck("username") }}} | {{{ $post->post_date }}}</h4>
+                                                <p class="card-text">{{{ $post->text }}}</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                            @endif
+                        @endforeach
                 </div>
             </div>
 
