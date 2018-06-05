ade35b21020101e0453594b3a9a66f839d835b1d
casper <64666@gmail.com>
2018-06-05 21:13:50 +0100

    channels / pages / events / groups / (Added cover & image)

--- app/views/discover/page.blade.php
+++ app/views/discover/page.blade.php
@@ -10,52 +10,8 @@
             @endif
             <div class="col-12" style="overflow-y:scroll;margin-top:60px;padding:10px;background:#efefef;height:100vh;">
                 @yield("content")
-                <div class="jumbotron jumbotron-fluid" style="height:20%;">
-                    <div class="container" style="height:40%;color:black;">
-                        <h1 class="display-3">Discover</h1>
-                        <p class="lead">Discover more content from people around the world!</p>
-                    </div>
-                </div></br>
-                <div class="row">
-                    <div class="col-4">
-                        <a href="{{ URL::route("photos") }}"><button class="btn btn-outline-info" style="width:100%;">Pictures</button></a>
-                    </div>
-                    <div class="col-4">
-                        <a href="{{ URL::route("videos") }}"><button class="btn btn-outline-info" style="width:100%;">Videos</button></a>
-                    </div>
-                    <div class="col-4">
-                        <a href="{{ URL::route("discover.gif") }}"><button class="btn btn-outline-info" style="width:100%;">Gif's</button></a>
-                    </div>
-                </div></br>
-                <div class="row">
-                    <div class="col-4">
-                        <a href="{{ URL::route("discover.page") }}"><button class="btn btn-outline-info" style="width:100%;">Pages</button></a>
-                    </div>
-                    <div class="col-4">
-                        <a href="{{ URL::route("discover.group") }}"><button class="btn btn-outline-info" style="width:100%;">Groups</button></a>
-                    </div>
-                    <div class="col-4">
-                        <a href="{{ URL::route("discover.channel") }}"><button class="btn btn-outline-info" style="width:100%;">Channels</button></a>
-                    </div>
-                </div></br>
-                <div class="card-columns">
-
-
-
-
-                    <div class="card">
-                        <img class="card-img-top" src="https://www.facesmash.co.uk/uploads/55529055162cf.png" alt="Card image cap">
-                        <div class="card-body">
-
-                            <h4 class="card-title">Facesmash</h4>
-                            <p class="card-text"></p>
-                        </div>
-                    </div>
-
-
-
-
-                </div>
-
+                @include("includes.discover-top")
             </div>
+
+        </div>
 @stop
