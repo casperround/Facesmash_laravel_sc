29d5c6c584bdb9bc123a995b970daa50a9dc870a
casper <64666@gmail.com>
2018-06-05 01:20:08 +0100

    updated head -> title

--- app/views/discover/photo.blade.php
+++ app/views/discover/photo.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.public', ["title" => "Public Photos", "sidebar" => false])
+@extends('layouts.public', ["title" => "Discover Photos", "sidebar" => false])
 
 @section("content")
     @if (Auth::check())
@@ -7,13 +7,18 @@
             <div class="col-md" style="overflow-y:scroll;margin-top:10px;padding:10px;background:#efefef;height:100vh;">
                 @include("includes.discover-top")
                 <div class="card-columns">
-                    @foreach(Posts::where("media_type", "=", "image")->get() as $post)
+                    @foreach(Posts::where("media_type", "=", "jpg" OR "media_type", "=", "JPG" OR "media_type", "=", "png" OR "media_type", "=", "PNG")->get() as $post)
                         <div class="col-md">
                             <div class="card">
                                 <img class="card-img-top" src="{{ URL::to($post->file_path) }}" alt="Card image cap">
                                 <div class="card-body" style="color: black">
                                     <h4 class="card-title">{{{ User::where("id", "=", $post->author_id)->first()->pluck("username") }}} | {{{ $post->post_date }}}</h4>
                                     <p class="card-text">{{{ $post->text }}}</p>
+                                </div>
+                                <div class="card-group" style="color:black;">
+                                    <div class="card">
+                                        <img style="width: 100%;height: auto;padding: 10px;" src="{{ $post->file_path }}">
+                                    </div>
                                 </div>
                             </div>
                         </div>
