6f1cad742e38cff4c7a00b057a4a0b84c89707c3
casper <64666@gmail.com>
2018-06-05 01:39:38 +0100

    updated head -> title

--- app/views/discover.blade.php
+++ app/views/discover.blade.php
@@ -9,13 +9,13 @@
             <div class="col-md" style="overflow-y:scroll;margin-top:10px;padding:10px;background:#efefef;height:100vh;">
                 @include("includes.discover-top")
                 <div class="card-columns">
-                        @foreach(Posts::where("visibility", "=", "4")->orderBy('post_time', 'DESC')->orderBy('post_date', 'DESC')->get() as $post)
+                        @foreach(Posts::where("visibility", "=", "1")->orderBy('post_time', 'DESC')->orderBy('post_date', 'DESC')->get() as $post)
 
                         @if ($post->media_type == 'text')
                         <div class="col-md">
                             <div class="card">
                                 <div class="card-body" style="color: black">
-                                    <h4 class="card-title">{{{ User::where("id", "=", $post->author_id)->first()->pluck("username") }}} | {{{ $post->post_date }}}</h4>
+                                    <h4 class="card-title">{{{ User::where("id", "=", $post->author_id)->pluck("username") }}} | {{{ $post->post_date }}}</h4>
                                     <p class="card-text">{{{ $post->text }}}</p>
                                 </div>
                             </div>
