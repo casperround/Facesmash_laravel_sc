6183f05b6a9bcbfc6904336570439f49c170bcef
casper <64666@gmail.com>
2018-06-05 20:20:03 +0100

    channels / pages / events / groups / (Added cover & image)

--- app/views/pages/userpage.blade.php
+++ app/views/pages/userpage.blade.php
@@ -89,7 +89,23 @@
                                             </option>
                                         </select>
                                     </div>
+                                    <input name="file_upload" class="form-control" type="file" onchange="readURL(this);" >
+                                    <img id="blah" src="#" style="box-shadow: 0px 0px 9px 0px rgba(0,0,0,0.75);margin: 20px;" alt="your image">
+                                    <script>
+                                        function readURL(input) {
+                                            if (input.files && input.files[0]) {
+                                                var reader = new FileReader();
 
+                                                reader.onload = function (e) {
+                                                    $('#blah')
+                                                        .attr('src', e.target.result)
+                                                        .width(150)
+                                                        .height(auto);
+                                                };
+                                                reader.readAsDataURL(input.files[0]);
+                                            }
+                                        }
+                                    </script>
                                     <button type="submit" name="login" value="Create Page" class="btn btn-primary">Create</button>
                                     {{ Form::token() }}
                                 </form>
