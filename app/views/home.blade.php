6569127311bb92c602e01cce206a1f1a150769a2
casper <64666@gmail.com>
2018-06-05 01:04:55 +0100

    updated head -> title

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -65,7 +65,7 @@
 
                 </div>
                 <div class="col">
-                        <input name="file_upload" class="form-control" type="file" onchange="readURL(this);" >
+                        <input name="file_upload" class="form-control" type="file" >
                         <img id="blah" src="#" style="box-shadow: 0px 0px 9px 0px rgba(0,0,0,0.75);margin: 20px;" alt="your image" />
                         <script>
                             function readURL(input) {
