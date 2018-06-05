69dd1fd08093e478493b3c1c5c02e1cc5eb98652
casper <64666@gmail.com>
2018-06-05 23:09:12 +0100

    channels / pages / events / groups / (Added cover & image)

--- app/views/pages/pageview.blade.php
+++ app/views/pages/pageview.blade.php
@@ -20,9 +20,49 @@
                                         </div>
                                     </div>
                                 @endforeach
+                                    <div class="Post_Container">
+                                        <form enctype="multipart/form-data" action="{{ URL::route("pages.createNewPost") }}" method="POST">
+                                            <div class="row">
+                                                <div class="col-8">
+                                                    <textarea name="home_post" style="width:100%;height:50px;resize: none;border-radius: 5px;background:#efefef;border-color: #5d3bae;" placeholder="Write something about your day..."></textarea>
+                                                    <div class="form-group">
+                                                        <select class="form-control" name="visibility">
+                                                            <option value="1">Public</option>
+                                                            <option value="2">Friends & Friends of friends</option>
+                                                            <option value="3">Friends</option>
+                                                            <option value="4" selected>Only me</option>
+                                                        </select>
+                                                    </div>
+                                                    <button type="submit" class="btn purp-button">Post</button>
+                                                </div>
+                                                <div class="col">
+                                                    <input name="file_upload" class="form-control" type="file" onchange="readURL(this);" >
+                                                    <img id="blah" src="#" style="box-shadow: 0px 0px 9px 0px rgba(0,0,0,0.75);margin: 20px;" alt="your image">
+                                                    <script>
+                                                        function readURL(input) {
+                                                            if (input.files && input.files[0]) {
+                                                                var reader = new FileReader();
+
+                                                                reader.onload = function (e) {
+                                                                    $('#blah')
+                                                                        .attr('src', e.target.result)
+                                                                        .width(150)
+                                                                        .height(auto);
+                                                                };
+                                                                reader.readAsDataURL(input.files[0]);
+                                                            }
+                                                        }
+                                                    </script>
+                                                    {{ Form::token() }}
+                                                </div>
+                                            </div>
+                                        </form>
+                                    </div>
                         </div>
 
+                        @if (Auth::check())
                     </div>
+                    @else
                 </div>
         </div>
     @endif
