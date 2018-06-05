71f7f13b9f3d17c0ea35f47994de5e5f7316410b
casper <64666@gmail.com>
2018-06-05 21:21:20 +0100

    channels / pages / events / groups / (Added cover & image)

--- app/views/discover.blade.php
+++ app/views/discover.blade.php
@@ -9,6 +9,16 @@
             <div class="col-md" style="overflow-y:scroll;margin-top:10px;padding:10px;background:#efefef;height:100vh;">
                 @include("includes.discover-top")
                 <div class="card-columns">
+                        @foreach(Pages::where("visibility", "=", "1")->get() as $pages)
+                            <div class="col-md">
+                                <a href="{{ URL::route("pagesview", $pages->unique_pagename) }}"><div class="card">
+                                        <img class="card-img-top" src="{{ URL::to($pages->banner_img_path) }}" alt="Card image cap">
+                                        <div class="card-body">
+                                            <center><p style="font-size:20px;font-weight:bold;color:black;" class="card-text">{{ $pages->unique_pagename }}</p></center>
+                                        </div>
+                                    </div></a>
+                            </div>
+                        @endforeach
                         @foreach(Posts::where("visibility", "=", "1")->orderBy('post_time', 'DESC')->orderBy('post_date', 'DESC')->get() as $post)
 
                         @if ($post->media_type == 'text')
