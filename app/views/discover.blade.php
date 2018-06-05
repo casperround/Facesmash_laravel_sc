c7721ab86df976aca0447fd7cce26b84f10b5557
casper <64666@gmail.com>
2018-06-05 01:27:13 +0100

    updated head -> title

--- app/views/discover.blade.php
+++ app/views/discover.blade.php
@@ -10,20 +10,25 @@
                 @include("includes.discover-top")
                 <div class="card-columns">
                     @foreach(Posts::all() as $post)
+                        @if ($post->media_type == 'text')
                         <div class="col-md">
                             <div class="card">
-                                @if ($post->media_type == "image")
-                                    <img class="card-img-top" src="{{ URL::to("") }}" alt="Card image cap">
-                                @elseif($post->media_type == "videos")
-                                    <video class="card-img-top" src="" controls alt="Card image cap"></video>
-                                @elseif ($post->media_type == "text")
-                                @endif
                                 <div class="card-body" style="color: black">
                                     <h4 class="card-title">{{{ User::where("id", "=", $post->author_id)->first()->pluck("username") }}} | {{{ $post->post_date }}}</h4>
                                     <p class="card-text">{{{ $post->text }}}</p>
                                 </div>
                             </div>
                         </div>
+                        @endif
+                        @if ($post->media_type == 'jpg' OR $post->media_type == 'png' OR $post->media_type == 'PNG' OR $post->media_type == 'JPG')
+                                <div class="col-md">
+                                    <img class="card-img-top" src="{{ URL::to($post->file_path) }}" alt="Card image cap">
+                                    <div class="card-body" style="color: black">
+                                        <h4 class="card-title">{{{ User::where("id", "=", $post->author_id)->pluck("username") }}} | {{{ $post->post_date }}}</h4>
+                                        <p class="card-text">{{{ $post->text }}}</p>
+                                    </div>
+                                </div>
+                        @endif
                     @endforeach
                 </div>
             </div>
