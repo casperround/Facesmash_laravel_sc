5b889e23a12ab297f5646d851dedd17163537c75
casper <64666@gmail.com>
2018-06-06 00:16:01 +0100

    added font awesome

--- app/views/includes/discover-top.blade.php
+++ app/views/includes/discover-top.blade.php
@@ -9,13 +9,13 @@
 
     <div class="row">
         <div class="col-4">
-            <a href="{{ URL::route("discover.photo") }}"><button class="btn btn-outline-info" style="width:100%;">Pictures<center><i style="color:#ED5565" class="far fa-file-alt"></i></center></button></a>
+            <a href="{{ URL::route("discover.photo") }}"><button class="btn btn-outline-info" style="width:100%;">Pictures<center><i style="color:#48CFAD" class="far fa-image"></i></center></button></a>
         </div>
         <div class="col-4">
-            <a href="{{ URL::route("discover.video") }}"><button class="btn btn-outline-info" style="width:100%;">Videos</button></a>
+            <a href="{{ URL::route("discover.video") }}"><button class="btn btn-outline-info" style="width:100%;">Videos<center><i style="color:#AC92EC" class="fas fa-video"></i></center></button></a>
         </div>
         <div class="col-4">
-            <a href="{{ URL::route("discover.gif") }}"><button class="btn btn-outline-info" style="width:100%;">Gif's</button></a>
+            <a href="{{ URL::route("discover.gif") }}"><button class="btn btn-outline-info" style="width:100%;">Gif's<center><i style="color:#48CFAD" class="far fa-image"></i></center></button></a>
         </div>
     </div>
 
@@ -23,13 +23,13 @@
 
     <div class="row">
         <div class="col-4">
-            <a href="{{ URL::route("discover.page") }}"><button class="btn btn-outline-info" style="width:100%;">Pages</button></a>
+            <a href="{{ URL::route("discover.page") }}"><button class="btn btn-outline-info" style="width:100%;">Pages<center><i style="color:#ED5565" class="far fa-file-alt"></i></center></button></a>
         </div>
         <div class="col-4">
-            <a href="{{ URL::route("discover.group") }}"><button class="btn btn-outline-info" style="width:100%;">Groups</button></a>
+            <a href="{{ URL::route("discover.group") }}"><button class="btn btn-outline-info" style="width:100%;">Groups<center><i style="color:#5D9CEC" class="fas fa-users"></i></center></button></a>
         </div>
         <div class="col-4">
-            <a href="{{ URL::route("discover.channel") }}"><button class="btn btn-outline-info" style="width:100%;">Channels</button></a>
+            <a href="{{ URL::route("discover.channel") }}"><button class="btn btn-outline-info" style="width:100%;">Channels<center><i style="color:#A0D468" class="fas fa-tv"></i></center></button></a>
         </div>
     </div>
     <br>
