80ece9a990696b4216868826d522c1817324d550
casper <64666@gmail.com>
2018-06-06 09:51:08 +0100

    Channel updates

--- app/views/discover/channel.blade.php
+++ app/views/discover/channel.blade.php
@@ -1,61 +1,26 @@
-@extends('layouts.public', ["title" => "Public Channels", "sidebar" => false])
+@extends('layouts.public', ["title" => "Discover Channels", "sidebar" => false])
 
 @section("content")
-
-    <style>
-
-    </style>
     @if (Auth::check())
         <div class="col-8" style="overflow-y:scroll;margin-top:60px;padding:10px;background:#efefef;height:100vh;">
             @endif
-            <div class="col-12" style="overflow-y:scroll;margin-top:60px;padding:10px;background:#efefef;height:100vh;">
-                @yield("content")
-        <div class="jumbotron jumbotron-fluid" style="height:20%;">
-            <div class="container" style="height:40%;color:black;">
-                <h1 class="display-3">Discover</h1>
-                <p class="lead">Discover more content from people around the world!</p>
+            <div class="col-md" style="overflow-y:scroll;margin-top:10px;padding:10px;background:#efefef;height:100vh;">
+                @include("includes.discover-top")
+                <div class="card-columns">
+                    @foreach(Channels::where("visibility", "=", "1")->get() as $channels)
+                        <div class="col-md">
+                            <a href="{{ URL::route("channelsview", $channels->unique_channelname) }}"><div class="card">
+                                    <img class="card-img-top" src="{{ URL::to($channels->banner_img_path) }}" alt="Card image cap">
+                                    <div class="card-body">
+                                        <center><p style="font-size:20px;font-weight:bold;color:black;" class="card-text">{{ $channels->unique_channelname }}</p></center>
+                                    </div>
+                                </div></a>
+                        </div>
+                    @endforeach
+                </div>
             </div>
-        </div></br>
-        <div class="row">
-            <div class="col-4">
-                <a href="{{ URL::route("photos") }}"><button class="btn btn-outline-info" style="width:100%;">Pictures</button></a>
-            </div>
-            <div class="col-4">
-                <a href="{{ URL::route("videos") }}"><button class="btn btn-outline-info" style="width:100%;">Videos</button></a>
-            </div>
-            <div class="col-4">
-                <a href="{{ URL::route("discover.gif") }}"><button class="btn btn-outline-info" style="width:100%;">Gif's</button></a>
-            </div>
-        </div></br>
-        <div class="row">
-            <div class="col-4">
-                <a href="{{ URL::route("discover.page") }}"><button class="btn btn-outline-info" style="width:100%;">Pages</button></a>
-            </div>
-            <div class="col-4">
-                <a href="{{ URL::route("discover.group") }}"><button class="btn btn-outline-info" style="width:100%;">Groups</button></a>
-            </div>
-            <div class="col-4">
-                <a href="{{ URL::route("discover.channel") }}"><button class="btn btn-outline-info" style="width:100%;">Channels</button></a>
-            </div>
-        </div></br>
-        <div class="card-columns">
 
-
-            @foreach(Channels::all() as $channels)
-                <a href="{{ URL::route("channelsview", $channels->unique_channelname) }}"><div class="card">
-                    <img class="card-img-top" src="{{ $channels->unique_channelname }}" alt="Card image cap">
-                    <div class="card-body">
-
-                        <center><p style="font-size:20px;font-weight:bold;color:black" class="card-text">{{ $channels->unique_channelname }}</p></center>
-                    </div>
-                    </div></a>
-            @endforeach
-
-
-
-
-
+            @if (Auth::check())
         </div>
-
-    </div>
+    @endif
 @stop
