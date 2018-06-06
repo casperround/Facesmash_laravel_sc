1de8286f4cf3e3502d29b0596432950b05c3ee6f
casper <64666@gmail.com>
2018-06-06 09:37:33 +0100

    Channel updates

--- app/views/channels/channelview.blade.php
+++ app/views/channels/channelview.blade.php
@@ -58,15 +58,15 @@
                                 @endif
                                 @foreach(Posts::where("relation_id", "=", $channels->id)->where("relation", "=", "channel")->orderBy('post_time', 'DESC')->orderBy('post_date', 'DESC')->get() as $post)
                                     @if ($post->media_type == 'mp4' OR $post->media_type == 'MP4')
-                                        <div class="col-md-4" style="display:inline-block;box-shadow: 0px 0px 20px 0px rgba(0,0,0,0.75);color:black;border-radius: 5px;margin-top:20px;">
+                                        <div class="col-md-4" style="display:inline-block;background:#A0D468;box-shadow: 0px 0px 20px 0px rgba(0,0,0,0.75);color:white;border-radius: 5px;margin-top:20px;">
                                             <div class="row" style="width:100%;margin:0px;position: relative;">
-                                                <div class="col-1">
+                                                <div class="col-4">
                                                     @if ($post->author_id == $channels->owner_id)
                                                         <img class="img" style="height:40px;width:40px;border-radius: 50px;" src="{{ URL::to($channels->channel_img_path) }}"/>
                                                     @else
                                                         <img class="img" style="height:40px;width:40px;border-radius: 50px;" src="{{URL::to(User::where("id", "=", $post->author_id)->limit(1)->pluck("profile_img_path"))}}"/>
                                                     @endif                                                </div>
-                                                <div class="col-2">
+                                                <div class="col-4">
                                                     <i style="color:#48CFAD" class="far fa-image"></i>
                                                     @if ($post->author_id == $channels->owner_id)
                                                         <span>{{ $channels->unique_channelname }}</span>
@@ -74,7 +74,7 @@
                                                         <span>{{User::where("id", "=", $post->author_id)->pluck("username");}}</span>
                                                     @endif
                                                 </div>
-                                                <div class="col-2">
+                                                <div class="col-4">
                                                     <span>{{ $post->post_date }}</span>
                                                 </div>
 
