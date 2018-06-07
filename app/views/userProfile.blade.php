6103d1dffeb4c6b98f793edf00500f8fac830173
casper <64666@gmail.com>
2018-06-07 23:37:54 +0100

    video view

--- app/views/userProfile.blade.php
+++ app/views/userProfile.blade.php
@@ -17,7 +17,9 @@
         border: 1px solid;
         border-color: #e5e6e9 #dfe0e4 #d0d1d5;
     }
-
+    header {
+        height:250px;
+    }
     .profile-card::before {
         content: '';
         height: 3px;
