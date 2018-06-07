9678c21273a1f6969bb0133d056ad47a488c3e72
casper <64666@gmail.com>
2018-06-08 00:17:17 +0100

    video view

--- app/views/userProfile.blade.php
+++ app/views/userProfile.blade.php
@@ -393,12 +393,87 @@
 
                 </div>
             <br>
-
-            <div class="profile-bio">
-                <p>{{{ $user->about }}}</p>
-            </div>
-
-
         </aside>
+        <style>
+            .Post_Container {
+                height:150px;
+                width:90%;
+                border-radius: 5px;
+                margin:10px;
+                padding:5px;
+            }
+            body ::-webkit-input-placeholder {
+                /* WebKit browsers */
+                font-family: 'Source Sans Pro', sans-serif;
+                color: black;
+                font-weight: 300;
+            }
+            body :-moz-placeholder {
+                /* Mozilla Firefox 4 to 18 */
+                font-family: 'Source Sans Pro', sans-serif;
+                color: black;
+                opacity: 1;
+                font-weight: 300;
+            }
+            body ::-moz-placeholder {
+                /* Mozilla Firefox 19+ */
+                font-family: 'Source Sans Pro', sans-serif;
+                color: black;
+                opacity: 1;
+                font-weight: 300;
+            }
+            body :-ms-input-placeholder {
+                /* Internet Explorer 10+ */
+                font-family: 'Source Sans Pro', sans-serif;
+                color: black;
+                font-weight: 300;
+            }
+            .purp-button {
+                background-color: #5d3bae;
+                color: white;
+            }
+            .purp-button:hover {
+                background-color: #423385;
+                color: white;
+            }
+        </style>
+        <div class="Post_Container">
+            <form enctype="multipart/form-data" action="{{ URL::route("home.createNewPost") }}" method="POST">
+                <div class="row">
+                    <div class="col-8">
+                        <textarea name="home_post" style="width:100%;height:50px;resize: none;border-radius: 5px;background:#efefef;border-color: #5d3bae;" placeholder="Write something about your day..."></textarea>
+                        <div class="form-group">
+                            <select class="form-control" name="visibility">
+                                <option value="1">Public</option>
+                                <option value="2">Friends & Friends of friends</option>
+                                <option value="3">Friends</option>
+                                <option value="4" selected>Only me</option>
+                            </select>
+                        </div>
+                        <button type="submit" class="btn purp-button">Post</button>
+                    </div>
+                    <div class="col">
+                        <input name="file_upload" class="form-control" type="file" onchange="readURL(this);" >
+                        <img id="blah" src="#" style="box-shadow: 0px 0px 9px 0px rgba(0,0,0,0.75);margin: 20px;" alt="your image">
+                        <script>
+                            function readURL(input) {
+                                if (input.files && input.files[0]) {
+                                    var reader = new FileReader();
+
+                                    reader.onload = function (e) {
+                                        $('#blah')
+                                            .attr('src', e.target.result)
+                                            .width(150)
+                                            .height(auto);
+                                    };
+                                    reader.readAsDataURL(input.files[0]);
+                                }
+                            }
+                        </script>
+                        {{ Form::token() }}
+                    </div>
+                </div>
+            </form>
+        </div>
     </div>
 @stop
