e0cbed4f12bb367af0e84e18d5a50396da5a09c2
casper <64666@gmail.com>
2018-06-07 23:33:00 +0100

    video view

--- app/views/userProfile.blade.php
+++ app/views/userProfile.blade.php
@@ -247,7 +247,7 @@
         <aside class="profile-card">
             <header>
                 <div class="user-profile">
-                    <img src="{{ URL::to($user->profile_img_path) }}">
+                    <img style="position:relative;" src="{{ URL::to($user->profile_img_path) }}">
                     <div class="username">Will Smith</div>
                     <div class="bio">
                         Senior UI Designer
