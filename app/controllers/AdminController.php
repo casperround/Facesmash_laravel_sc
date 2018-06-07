4d703c16e24146fabb199fd27599c80014ce3589
Rod <rod@rodlucas.uk>
2018-06-07 14:59:57 +0100

    working on admin user management

--- app/controllers/AdminController.php
+++ app/controllers/AdminController.php
@@ -40,4 +40,54 @@ class AdminController extends BaseController
 
     }
 
+    public function userPageUpdateInfo($id) {
+
+        $user = User::where("id", "=", $id)->get()->first();
+
+        if (Input::has("username")) {
+
+            $username = Input::get("username");
+            $email = Input::get("email");
+            $first_name = Input::get("first_name");
+            $last_name = Input::get("last_name");
+            $date_birth = Input::get("date_birth");
+            $country = Input::get("country");
+            $theme = Input::get("theme");
+            $gender = Input::get("gender");
+            $about = Input::get("about");
+
+            if (User::where("username", "=", $username)->count() > 0) {
+                return Redirect::route("admin.user", [$user->id]);
+            }
+
+
+            $user->update([
+                "username" => $username,
+                "email" => $email,
+                "first_name" => $first_name,
+                "last_name" => $last_name,
+                "date_birth" => $date_birth,
+                "country" => $country,
+                "theme" => $theme,
+                "gender" => $gender,
+                "about" => $about
+            ]);
+
+        }
+
+        if (Input::has("role")) {
+
+
+
+        }
+
+        if (Input::has("twitter")) {
+
+
+
+        }
+
+        return Redirect::route("admin.user", [$user->id]);
+    }
+
 }
--- app/views/admin/users/user.blade.php
+++ app/views/admin/users/user.blade.php
@@ -10,12 +10,84 @@
                     <div class="m-portlet__head-caption">
                         <div class="m-portlet__head-title">
                             <h3 class="m-portlet__head-text">
-                                User Info
+                                View/Edit User Info
                             </h3>
                         </div>
                     </div>
                 </div>
                 <div class="m-portlet__body">
+                    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#generalDataModal">General Data</button>
+                    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#socialMediaDataModal">Social Media Data</button>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="m-portlet m-portlet--unair">
+                <div class="m-portlet__head">
+                    <div class="m-portlet__head-caption">
+                        <div class="m-portlet__head-title">
+                            <h3 class="m-portlet__head-text">
+                                User Graphics
+                            </h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="m-portlet__body">
+                    <p>Users Profile Image</p>
+                    <img height="70" src="{{ URL::to($user->profile_img_path) }}">
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="m-portlet m-portlet--unair">
+                <div class="m-portlet__head">
+                    <div class="m-portlet__head-caption">
+                        <div class="m-portlet__head-title">
+                            <h3 class="m-portlet__head-text">
+                                Admin Data/Functions
+                            </h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="m-portlet__body">
+                    <form class="form-control">
+                        <div class="form-group">
+                            <label>Account Created</label>
+                            <input readonly class="form-control" value="{{{ $user->created_at }}}">
+                        </div>
+                        <div class="form-group">
+                            <label>Account Last Updated</label>
+                            <input readonly class="form-control" value="{{{ $user->updated_at }}}">
+                        </div>
+                    </form>
+                    <br>
+                    <form class="form-control" src="" method="post">
+                        <div class="form-group">
+                            <label>User Role</label>
+                            <select class="form-control" name="role">
+                                <option value="1">Standard User</option>
+                                <option value="0">Suspended User</option>
+                                <optgroup label=""></optgroup>
+                                <option value="2">Admin</option>
+                            </select>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="modal fade" id="generalDataModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">General Data</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body" style="height: 500px; overflow-y: scroll">
                     <form class="form-control" src="" method="post">
                         <div class="form-group">
                             <label>Username</label>
@@ -53,26 +125,27 @@
                             <label>About</label>
                             <textarea rows="5" class="form-control" name="about">{{{ $user->about }}}</textarea>
                         </div>
-                        <div class="form-group">
-                            <label>Youtube</label>
-                            <input class="form-control" name="youtube" value="{{{ $user->youtube }}}">
-                        </div>
+                        <button type="submit" class="btn btn-primary">Update</button>
+                        {{ Form::token() }}
                     </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
                 </div>
             </div>
         </div>
-        <div class="col-md-4">
-            <div class="m-portlet m-portlet--unair">
-                <div class="m-portlet__head">
-                    <div class="m-portlet__head-caption">
-                        <div class="m-portlet__head-title">
-                            <h3 class="m-portlet__head-text">
-                                Users Social Media
-                            </h3>
-                        </div>
-                    </div>
+    </div>
+
+    <div class="modal fade" id="socialMediaDataModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Social Media Data</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
                 </div>
-                <div class="m-portlet__body">
+                <div class="modal-body" style="height: 500px; overflow-y: scroll">
                     <form class="form-control" src="" method="post">
                         <div class="form-group">
                             <label>Youtube</label>
@@ -94,10 +167,14 @@
                             <label>Website</label>
                             <input class="form-control" name="website" value="{{{ $user->website }}}">
                         </div>
+                        <button type="submit" class="btn btn-primary">Update</button>
+                        {{ Form::token() }}
                     </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
                 </div>
             </div>
         </div>
     </div>
-
 @stop
