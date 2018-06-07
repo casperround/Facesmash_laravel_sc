9a6764076b423deafb8212864f90d81ee95f2885
casper <64666@gmail.com>
2018-06-07 13:07:34 +0100

    photo view

---
+++ app/views/discover/photoview.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.public', ["title" => $post->text, "sidebar" => false])
+@section("in-head")
+    <meta property="og:url" content="https://www.facesmash.co.uk/page/{{ $post->post_id }}">
+    {{--<meta property="og:type" content="website">--}}
+    <meta property="og:title" content="{{URL::to(User::where("id", "=", $post->author_id)->limit(1)->pluck("username"))}}">
+    <meta property="og:description" content="{{ $post->text }}">
+    <meta property="og:image" content="{{ URL::to($post->file_path) }}">
+    {{--<meta name="theme-color" content="#ffffff">--}}
+    {{--<meta content="Casper Round" name="author">--}}
+@stop
+@section("content")
+    @if (Auth::check())
+        <div class="col-8" style="overflow-y:scroll;margin-top:60px;padding:10px;background:#efefef;height:100vh;">
+            @else
+                <div class="col-12" style="margin-top:60px;padding:10px;background:#efefef;height:100vh;">
+
+                    <div class="container-fluid">
+                        @endif
+                        <div class="col-md" style="overflow-y:scroll;margin-top:10px;padding:10px;background:#efefef;height:100vh;">
+                            <div class="card">
+                                <div class="card-body">
+                                    <center>
+                                        <img style="height: 400px!important;" class="card-img-top" src="{{ URL::to($post->file_path) }}" alt="Card image cap">
+                                        <i style="color:#ED5565" class="far fa-file-alt"></i>
+                                        <p style="font-size:20px;font-weight:bold;color:black" class="card-text">{{ $post->text }}</p>
+                                    </center>
+                                </div>
+                            </div>
+                            @endforeach
+
+                        </div>
+
+                        @if (Auth::check())
+                    </div>
+                    @else
+                </div>
+        </div>
+    @endif
+@stop
