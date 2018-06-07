1f3e6afbf062902afc8b0c65d0701829033ccea9
casper <64666@gmail.com>
2018-06-08 00:34:40 +0100

    video view

--- app/views/userProfile.blade.php
+++ app/views/userProfile.blade.php
@@ -350,7 +350,7 @@
 @stop
 
 @section("content")
-    <div class="col-8" style="overflow-y:scroll;margin-top:60px;padding:10px;background:#efefef;height:100vh;color:black;">
+    <div class="col-md" style="overflow-y:scroll;margin-top:60px;padding:10px;background:#efefef;height:100vh;color:black;">
         <aside class="profile-card">
             <header style="height:250px">
                 <div class="user-profile">
