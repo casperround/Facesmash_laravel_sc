1aaa460b5cea73cfb12f2f2d7278c1e80252f750
casper <64666@gmail.com>
2018-06-07 23:52:25 +0100

    video view

--- app/views/userProfile.blade.php
+++ app/views/userProfile.blade.php
@@ -382,7 +382,7 @@
 
             </header>
             <ul class="profile-social-links">
-                <div class="social-btns"><a style="    padding: 2px 10px;" class="btn facebook" href="#"><i  class="fa fa-facebook"></i></a><a style="    padding: 3px 10px;" class="btn twitter" href="#"><i class="fa fa-twitter"></i></a><a  style="    padding: 3px 10px;" class="btn google" href="#"><i class="fa fa-google"></i></a><a style="    padding: 3px 10px;" class="btn dribbble" href="#"><i class="fa fa-dribbble"></i></a><a style="    padding: 3px 10px;" class="btn skype" href="#"><i class="fa fa-skype"></i></a>
+                <div class="social-btns"><a class="btn facebook" href="#"><i  class="fab fa-facebook-f"></i></a><a  class="btn twitter" href="#"><i class="fab fa-twitter-f"></i></a><a   class="btn google" href="#"><i class="fab fa-google-f"></i></a><a class="btn dribbble" href="#"><i class="fab fa-dribble-f"></i></a><a class="btn skype" href="#"><i class="fab fa-skype-f"></i></a>
                 </div>
                 @if ($user->youtube != "")
                     <li>
