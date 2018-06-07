e56e741fe096caaaa38d6c26634fb82333b7edd1
casper <64666@gmail.com>
2018-06-07 23:28:53 +0100

    video view

--- app/views/userProfile.blade.php
+++ app/views/userProfile.blade.php
@@ -172,7 +172,7 @@
 
     .user-profile {
         margin: auto;
-        width: 100%;
+        width: 25em;
         height: 11em;
         background: #fff;
         border-radius: .3em;
@@ -245,34 +245,33 @@
 @section("content")
     <div class="col-8" style="overflow-y:scroll;margin-top:60px;padding:10px;background:#efefef;height:100vh;color:black;">
         <aside class="profile-card">
-            <h1 class="title-pen"> User Profile <span>UI</span></h1>
-            <div class="user-profile">
-                <img class="avatar" src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTF_erFD1SeUnxEpvFjzBCCDxLvf-wlh9ZuPMqi02qGnyyBtPWdE-3KoH3s" alt="Ash" />
-                <div class="username">Will Smith</div>
-                <div class="bio">
-                    Senior UI Designer
-                </div>
-                <div class="description">
-                    I use to design websites and applications
-                    for the web.
-                </div>
-                <ul class="data">
-                    <li>
-                        <span class="entypo-heart"> 127</span>
-                    </li>
-                    <li>
-                        <span class="entypo-eye"> 853</span>
-                    </li>
-                    <li>
-                        <span class="entypo-user"> 311</span>
-                    </li>
-                </ul>
-            </div>
-
             <header>
                 <a href="#">
                     <img src="{{ URL::to($user->profile_img_path) }}">
                 </a>
+                <h1 class="title-pen"> User Profile <span>UI</span></h1>
+                <div class="user-profile">
+                    <img class="avatar" src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTF_erFD1SeUnxEpvFjzBCCDxLvf-wlh9ZuPMqi02qGnyyBtPWdE-3KoH3s" alt="Ash" />
+                    <div class="username">Will Smith</div>
+                    <div class="bio">
+                        Senior UI Designer
+                    </div>
+                    <div class="description">
+                        I use to design websites and applications
+                        for the web.
+                    </div>
+                    <ul class="data">
+                        <li>
+                            <span class="entypo-heart"> 127</span>
+                        </li>
+                        <li>
+                            <span class="entypo-eye"> 853</span>
+                        </li>
+                        <li>
+                            <span class="entypo-user"> 311</span>
+                        </li>
+                    </ul>
+                </div>
 
                 <h1>{{{ $user->first_name }}} {{{ $user->last_name }}}</h1>
                 <h2>{{{ $user->username }}}</h2>
