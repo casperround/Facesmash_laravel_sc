1dd17cafd9292186bf0353ce702889c460c69a23
casper <64666@gmail.com>
2018-06-07 12:47:18 +0100

    page meta update

--- app/views/includes/sitewidemeta.blade.php
+++ app/views/includes/sitewidemeta.blade.php
@@ -3,11 +3,11 @@
 <meta name="Description" content="Facesmash - Sign up now!">
 <meta name="Keywords" content="facesmash,social,network,networking,chat,upload,pictures,media,picture,video,videos">
 
-<meta property="og:url" content="https://www.facesmash.co.uk">
+{{--<meta property="og:url" content="https://www.facesmash.co.uk">--}}
 <meta property="og:type" content="website"/>
-<meta property="og:title" content="Login | Facesmash">
-<meta property="og:description" content="Facesmash | Sign up now!">
-<meta property="og:image" content="{{ URL::to("") }}">
+{{--<meta property="og:title" content="Login | Facesmash">--}}
+{{--<meta property="og:description" content="Facesmash | Sign up now!">--}}
+{{--<meta property="og:image" content="{{ URL::to("") }}">--}}
 <meta name="theme-color" content="#ffffff">
 <meta content="Casper Round" name="author">
 
