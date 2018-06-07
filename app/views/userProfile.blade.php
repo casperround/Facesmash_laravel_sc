d6011f5e9e5045a6c67f91665ea6fc12735754d2
casper <64666@gmail.com>
2018-06-08 00:45:52 +0100

    video view

--- app/views/userProfile.blade.php
+++ app/views/userProfile.blade.php
@@ -351,11 +351,13 @@
 
 @section("content")
     @if (Auth::check())
-        <div class="col-8" style="overflow-y:scroll;margin-top:60px;padding:10px;background:#efefef;height:100vh;color:black;">
+        <div class="col-8" style="overflow-y:scroll;margin-top:60px;padding:10px;background:#efefef;height:100vh;">
             @else
-                <div class="container-fluid">
-                    @endif
-                    <div class="col-8" style="overflow-y:scroll;margin-top:60px;padding:10px;background:#efefef;height:100vh;color:black;">
+                <div class="col-12" style="margin-top:60px;padding:10px;background:#efefef;height:100vh;">
+
+                    <div class="container-fluid">
+                        @endif
+                        <div class="col-md" style="overflow-y:scroll;margin-top:10px;padding:10px;background:#efefef;height:100vh;">
         <aside class="profile-card">
             <header style="height:250px">
                 <div class="user-profile">
@@ -535,5 +537,6 @@
                 </div>
             @endif
         @endforeach
+                    </div>
     </div>
 @stop
