6b3e5b3edfc0bd48660ba16c280382735b569f6a
Rod <rod@rodlucas.uk>
2018-06-07 17:43:50 +0100

    working on graphs

--- app/views/admin/index.blade.php
+++ app/views/admin/index.blade.php
@@ -77,49 +77,7 @@
     </div>
 
     <div class="row">
-        <div class="col-xl-4">
-            <div class="m-portlet m-portlet--full-height  m-portlet--unair">
-                <div class="m-portlet__head">
-                    <div class="m-portlet__head-caption">
-                        <div class="m-portlet__head-title">
-                            <h3 class="m-portlet__head-text">
-                                Latest 100 Users
-                            </h3>
-                        </div>
-                    </div>
-                </div>
-                <div class="m-portlet__body">
-                    <div class="tab-content">
-                        <div class="tab-pane active" id="m_widget4_tab1_content">
-                            <div class="m-widget4">
-                                @foreach($newest100Users as $user)
-                                    <div class="m-widget4__item">
-                                        <div class="m-widget4__img m-widget4__img--pic">
-                                            <img src="{{ $user->profile_img_path }}" alt="">
-                                        </div>
-                                        <div class="m-widget4__info">
-                                            <span class="m-widget4__title">
-                                                {{{ $user->username }}}
-                                            </span>
-                                            <br>
-                                            <span class="m-widget4__sub">
-                                                {{{ $user->email }}}
-                                            </span>
-                                        </div>
-                                        <div class="m-widget4__ext">
-                                            <a href="#"  class="m-btn m-btn--pill m-btn--hover-brand btn btn-sm btn-secondary">
-                                                View
-                                            </a>
-                                        </div>
-                                    </div>
-                                @endforeach
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-4">
+        <div class="col-md">
             <div class="m-portlet m-portlet--full-height  m-portlet--unair">
                 <div class="m-portlet__head">
                     <div class="m-portlet__head-caption">
@@ -137,7 +95,7 @@
                 </div>
             </div>
         </div>
-        <div class="col-md-4">
+        <div class="col-md">
             <div class="m-portlet m-portlet--full-height  m-portlet--unair">
                 <div class="m-portlet__head">
                     <div class="m-portlet__head-caption">
