e4ea8519d19783575ab0fca14ea2f5c8120cfa64
casper <64666@gmail.com>
2018-06-08 14:10:30 +0100

    video view

--- app/views/channels/channelview.blade.php
+++ app/views/channels/channelview.blade.php
@@ -106,7 +106,7 @@
                                             </div>
                                             <div class="card-group" style="background:#efefef;border:0px;color:black;">
                                                 <div class="card" style="background:#efefef;border:0px;">
-                                                    <video id="my-video" class="video-js" controls preload="auto" width="100%" height="auto"
+                                                    <video id="my-video" class="video-js" controls preload="auto" width="100%" height="auto" style="width:100%;height:auto;position: relative;"
                                                            poster="MY_VIDEO_POSTER.jpg" data-setup="{}">
                                                         <source src="{{ URL::to($post->file_path) }}" type='video/mp4'>
                                                         <source src="{{ URL::to($post->file_path) }}" type='video/webm'>
